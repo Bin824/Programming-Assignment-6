6e1438e36646a4d7c50a31eabe11605278cd8fe4
Bin824 <zomberrylol@gmail.com>
2025-02-20 12:18:06 -0500

    .

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Email: bin.ni81@myhunter.cuny.edu
 Resources: None
 """
 
-def find_longest_word(input_filename, output_filename):
+def find_longest_words(input_filename, output_filename):
     """
     This function reads each line of the input file and writes the longest word
     into the output file. In case there are multiple longest words, the first one is written.
@@ -14,10 +14,14 @@ def find_longest_word(input_filename, output_filename):
         for line in infile:
             words = line.split()
             if words:
+                # Find the longest word in the line (the first one in case of ties)
                 longest_word = max(words, key=len)
                 outfile.write(longest_word + '\n')
+            else:
+                # If the line is empty, write an empty line
+                outfile.write('\n')
 
 if __name__ == "__main__":
-    input_file = input("Enter the input file name: ")
-    output_file = input("Enter the output file name: ")
-    find_longest_word(input_file, output_file)
+    input_file = input("Enter your input file name: ")
+    output_file = input("Enter your output file name: ")
+    find_longest_words(input_file, output_file)
